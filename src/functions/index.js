3b652d62f6aecfa3be0a51e115fd8a732c2d5787
michaelha <michaelha@unity3d.com>
2019-07-11 17:46:53 -0700

    Add attach library function

--- src/functions/index.js
+++ src/functions/index.js
@@ -20,6 +20,16 @@ module.exports = function (fractal) {
         },
         create_attribute(attributes) {
             return new drupalAttribute(attributes);
+        },
+        attach_library(library) {
+            if (typeof library === 'string' || library instanceof String) {
+                const yml = 'libraries/' + library.split('/')[0];
+                const lib = library.split('/')[1];
+                const config = yaml.safeLoad(fs.readFileSync(yml.concat('.libraries.yml'), 'utf8'));
+                return '<script src="' + Object.keys(config[lib].js)[0] + '"></script>';
+            } else {
+                console.error('Argument must be a string.');
+            }
         }
     }
 
